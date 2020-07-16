a9deee15ecef82e798a0e095ff07b3ce0e249f95
Tony Narlock <tony@git-pull.com>
2020-07-16 06:03:13 -0500

    Version number: p0 -> post1

--- graphene_django/__init__.py
+++ graphene_django/__init__.py
@@ -1,6 +1,6 @@
 from .fields import DjangoConnectionField
 from .types import DjangoObjectType
 
-__version__ = "2.4.0p0"
+__version__ = "2.4.0post1"
 
 __all__ = ["__version__", "DjangoObjectType", "DjangoConnectionField"]
