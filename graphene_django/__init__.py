c1c4160193dceacd0eca6f5e72facb744f6f24ea
Tony Narlock <tony@git-pull.com>
2020-08-05 06:09:18 -0500

    Update graphene-django 2.4.0post2

--- graphene_django/__init__.py
+++ graphene_django/__init__.py
@@ -1,6 +1,6 @@
 from .fields import DjangoConnectionField
 from .types import DjangoObjectType
 
-__version__ = "2.4.0post1"
+__version__ = "2.4.0post2"
 
 __all__ = ["__version__", "DjangoObjectType", "DjangoConnectionField"]
