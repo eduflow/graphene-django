09454d698b5d4a9a4ef7ef106276d4657d0c7ed5
Tony Narlock <tony@git-pull.com>
2020-08-05 06:06:05 -0500

    chore(JSONField): Update for django 3.1+
    This field becomes db-agnostic in 3.1+, raising a deprecation warning.
    The old import path will be deprecated completely in django 4.0.

--- graphene_django/compat.py
+++ graphene_django/compat.py
@@ -3,12 +3,16 @@ class MissingType(object):
 
 
 try:
+    try:  # Django 3.1+ has universal JSONField, old import path will deprecate in 4.0
+        from django.db.models import JSONField
+    except ImportError:
+        from django.contrib.postgres.fields import JSONField
+
     # Postgres fields are only available in Django with psycopg2 installed
     # and we cannot have psycopg2 on PyPy
     from django.contrib.postgres.fields import (
         ArrayField,
         HStoreField,
-        JSONField,
         RangeField,
     )
 except ImportError:
